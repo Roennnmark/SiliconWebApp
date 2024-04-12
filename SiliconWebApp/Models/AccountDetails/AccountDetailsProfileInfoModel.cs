d33ea9fbb94597d7374ade941f9e08946c272b28
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-04-12 16:36:43 +0200

    112 vet inte vad jag gör

--- SiliconWebApp/Models/AccountDetails/AccountDetailsProfileInfoModel.cs
+++ SiliconWebApp/Models/AccountDetails/AccountDetailsProfileInfoModel.cs
@@ -5,5 +5,5 @@ public class AccountDetailsProfileInfoModel
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string ProfileImageUrl { get; set; } = "avatar.svg";
+    public string ProfileImageUrl { get; set; } = "avatar.jpg";
 }
